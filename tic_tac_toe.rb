7c602e9c77bb40d6cb54c1209b09d4c03c3443d5
Yeap Yong Tuck <max@Yeaps-MacBook-Air.local>
2022-05-03 23:15:08 +0800

    Change Board class's name to Grid and change the methods in it into class methods. Game is now playable if the user follows the rules.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class Player
-  def initialize
-    @board = Board.new
+  attr_accessor :board
 
+  def initialize
     print "Player 1, please input your name\n"
 
     @player1_name = gets.chomp
@@ -11,32 +11,32 @@ class Player
     @player2_name = gets.chomp
   end
 
-  def player_symbol
+  def player_marker
     print "#{@player1_name}, please choose between 'O' or 'X'\n"
 
-    @player1_symbol = gets.chomp
+    @player1_marker = gets.chomp
 
-    @player2_symbol = ''
+    @player2_marker = ''
 
-    if @player1_symbol == 'O'
-      @player2_symbol = 'X'
-      print "#{@player2_symbol}, your symbol is 'X'\n"
+    if @player1_marker == 'O'
+      @player2_marker = 'X'
+      print "#{@player2_name}, your symbol is 'X'\n"
     else
-      @player2_symbol = 'O'
-      print "#{@player2_symbol}, your symbol is 'O'\n"
+      @player2_marker = 'O'
+      print "#{@player2_name}, your symbol is 'O'\n"
     end
   end
 
   def player_move (player_name, symbol)
-    @board.display_board
+    Grid.display_grid
 
     print "#{player_name}, Choose where you want to put\n"
 
     move = gets.chomp
 
-    @board.instance_variables.map { |test|
-      if @board.instance_variable_get(test) == move
-        @board.instance_variable_set(test, symbol)
+    Grid.instance_variables.map { |test|
+      if Grid.instance_variable_get(test) == move
+        Grid.instance_variable_set(test, symbol)
       end
     }
   end
@@ -45,41 +45,43 @@ class Player
     i = 0
 
     while i < 5
-      player_move(@player1_name, @player1_symbol)
-      @board.check_for_win(@player1_name)
-      player_move(@player2_name, @player2_symbol)
-      @board.check_for_win(@player2_name)
+      player_move(@player1_name, @player1_marker)
+      Grid.check_for_win(@player1_name)
+      player_move(@player2_name, @player2_marker)
+      Grid.check_for_win(@player2_name)
       i += 1
     end
   end
 end
 
-class Board
-  attr_accessor :a, :b, :c, :d, :e, :f, :g, :h, :i
+class Grid
+  @a = 'a'
+  @b = 'b'
+  @c = 'c'
+  @d = 'd'
+  @e = 'e'
+  @f = 'f'
+  @g = 'g'
+  @h = 'h'
+  @i = 'i'
 
-  def initialize
-    @a = 'a'
-    @b = 'b'
-    @c = 'c'
-    @d = 'd'
-    @e = 'e'
-    @f = 'f'
-    @g = 'g'
-    @h = 'h'
-    @i = 'i'
+  class << self
+    attr_accessor :a, :b, :c, :d, :e, :f, :g, :h, :i
   end
 
-  def display_board
-    puts "#{@a}|#{@b}|#{@c}\n#{@d}|#{@e}|#{@f}\n#{@g}|#{@h}|#{@i}"
+  def self.display_grid
+    puts "\n#{@a}|#{@b}|#{@c}\n#{@d}|#{@e}|#{@f}\n#{@g}|#{@h}|#{@i}"
   end
 
-  def check_for_win(player)
+  def self.check_for_win(player)
     array = [
       [@a, @b, @c], [@a, @d, @g], [@b, @e, @h], [@c, @f, @i],
       [@d, @e, @f], [@g, @h, @i], [@g, @e, @c], [@a, @e, @i]
     ]
+
     array.each { |combination|
       if combination.uniq.length == 1
+        self.display_grid
         print "#{player} Win!"
         exit
       end
@@ -88,27 +90,52 @@ class Board
 end
 
 players = Player.new
-players.player_symbol
+players.player_marker
 players.start_game
 
 
 
 
 
-# create player 1
-# ask for name
-# ask for chess symbol OR choose "O" or "X"
-# Repeat above steps for player 2
-# create a board to play
-# announce that player 1 will start first
+# # create player 1
+# # ask for name
+# # ask for chess symbol OR choose "O" or "X"
+# # Repeat above steps for player 2
+# # create a board to play
+# # announce that player 1 will start first
 
-# loop {
-#   player_move {
-# player 1 input where he wants to put the chess
-# board updates
-# check for winning status (if abc, adg, etc. has the same value)
-# announce #{name} wins
-# }
-# }
+# # loop {
+# #   player_move {
+# # player 1 input where he wants to put the chess
+# # board updates
+# # check for winning status (if abc, adg, etc. has the same value)
+# # announce #{name} wins
+# # }
+# # }
 
-# Create Player 1 and 2 in just one instance(initialization)
+# # Create Player 1 and 2 in just one instance(initialization)
+
+# # Change display board or board class into class method
+
+# # Player 1 Name
+# player_1 = player.new
+# print input name
+# player_name = gets
+# print [player_name] input name
+# player_marker = gets
+
+# # Display the grid
+# display_grid
+# # Player 1 Move
+# player1.move(1)
+# if variable.value contains 1 then variable = player_marker
+# Grid.display_grid
+
+# player_1.check_for_win
+# # Check for win
+# if combination == true then Grid.display_grid and announce player_name win
+# # Display the grid
+# # Player 2 Move
+# # Check for win
+
+# # Grid Class - Use class variables instead of instance variable. Don't create new instance for this class.
