bcbd7854d940561e72e9569fa30f923ce2feecf1
Yeap Yong Tuck <max@Yeaps-MacBook-Air.local>
2022-05-09 18:52:52 +0800

    Add Game class to reduce dependencies between classes. Refactor code to redelegate responsibilities of each classes.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,136 +1,138 @@
 # frozen_string_literal: true
-
 require 'colorize'
 
 # Get the values of each of the variables in the Grid
 module ValuesArray
-  def current_values_array
-    Grid.instance_variables.map { |value| Grid.instance_variable_get(value) }
+  def current_values_array (grid)
+    grid.instance_variables.map { |value| grid.instance_variable_get(value) }
   end
 end
 
 # This class represents the players of the tic-tac-toe game
-class Players
+class Player
   include ValuesArray
 
-  def initialize
-    @player1_marker = ''
+  attr_reader :marker
 
-    @player2_marker = ''
+  @current_number_of_players = 0
 
-    puts 'Player 1, please input your name', '(Alphabets only. Special characters will be removed)'.light_black
-
-    @player1_name = gets.chomp.gsub(/[^0-9A-Za-z]/, '')
-
-    puts 'Player 2, please input your name', '(Alphabets only. Special characters will be removed)'.light_black
-
-    @player2_name = gets.chomp.gsub(/[^0-9A-Za-z]/, '')
-  end
-
-  def start_game
-    player1_marker_set
-    player2_marker_set
-
-    loop do
-      player_move(@player1_name, @player1_marker)
-      Grid.check_for_win(@player1_name, @player1_marker)
-      player_move(@player2_name, @player2_marker)
-      Grid.check_for_win(@player2_name, @player2_marker)
-    end
-  end
-
-  private
-
-  def player1_marker_set
-    puts "#{@player1_name}, please input 'O' or 'X' to select your marker"
-
-    @player1_marker = gets.chomp.upcase
-
-    player1_marker_check_exception
-  end
-
-  def player1_marker_check_exception
-    begin
-      raise 'Input is not valid. Please try again.'.red if (@player1_marker != 'O') && (@player1_marker != 'X')
-
-    rescue => e
-      puts e.to_s.red
-      player1_marker_set
-    end
-  end
-
-  def player2_marker_set
-    if @player1_marker == 'O'
-      @player2_marker = 'X'
-      puts "#{@player2_name}, your marker is 'X'"
-    else
-      @player2_marker = 'O'
-      puts "#{@player2_name}, your marker is 'O'"
-    end
-  end
-
-  def player_move (player_name, marker)
-    Grid.display
-
-    puts "#{player_name}(#{marker}), please mark your move by inputting one of the letters from the grid"
-
-    move = gets.chomp.downcase
-
-    # Check for exception in user input
-    if current_values_array.include?(move) == false
-      puts 'Input is not valid. Please try again.'.red
-      player_move(player_name, marker)
+    class << self
+      attr_accessor :current_number_of_players
     end
 
-    Grid.instance_variables.map do |value| 
-      if Grid.instance_variable_get(value) == move
-        Grid.instance_variable_set(value, marker)
-      end
-    end
+  def initialize (game, marker)
+    Player.current_number_of_players += 1
+    @game = game
+    @marker = marker
+    puts "Player #{Player.current_number_of_players}, please input your name", "(Alphabets and Numbers only)"
+    @name = gets.chomp.gsub(/[^0-9A-Za-z]/, '')
+  end
+
+  def to_s
+    "#{@name}"
   end
 end
 
 # This class represents the grid that the tic-tac-toe game is going to be played on
 class Grid
-  @a = 'a'
-  @b = 'b'
-  @c = 'c'
-  @d = 'd'
-  @e = 'e'
-  @f = 'f'
-  @g = 'g'
-  @h = 'h'
-  @i = 'i'
+  include ValuesArray
+  attr_reader :a, :b, :c, :d, :e, :f, :g, :h, :i
 
-  class << self
-    include ValuesArray
-    attr_reader :a, :b, :c, :d, :e, :f, :g, :h, :i
+  def initialize
+    @a = 'a'
+    @b = 'b'
+    @c = 'c'
+    @d = 'd'
+    @e = 'e'
+    @f = 'f'
+    @g = 'g'
+    @h = 'h'
+    @i = 'i'
   end
 
-  def self.display
+  def display
     puts "#{@a} | #{@b} | #{@c}\n--+---+--\n#{@d} | #{@e} | #{@f}\n--+---+--\n#{@g} | #{@h} | #{@i}"
-  end
-
-  def self.check_for_win(player_name, marker)
-    array = [[@a, @b, @c], [@a, @d, @g], [@b, @e, @h], [@c, @f, @i],
-             [@d, @e, @f], [@g, @h, @i], [@g, @e, @c], [@a, @e, @i]]
-
-    default_values_array = ("a".."i").to_a
-
-    array.each do |combination|
-      if combination.uniq.length == 1
-        display
-        puts "#{player_name}(#{marker}) is the winner!".green
-        exit
-
-      elsif (current_values_array - default_values_array).length == 9
-        display
-        puts "It's a draw!"
-        exit
-      end
-    end
   end
 end
 
-players = Players.new
-players.start_game
+class Game
+  include ValuesArray
+
+  def initialize(player_1_class, player_2_class)
+    @players = [player_1_class.new(self, "X"), player_2_class.new(self, "O")].shuffle
+    @grid = Grid.new
+    @current_player_index = 0
+    puts "#{current_player} (#{current_player.marker}) goes first."
+  end
+
+  def current_player
+    @players[@current_player_index]
+  end
+
+  def other_player_index
+    1 - @current_player_index
+  end
+
+  def switch_players!
+    @current_player_index = other_player_index
+  end
+
+  def start_game
+    loop do
+      player_move(current_player)
+      
+      if check_for_win?(current_player)
+        @grid.display
+        puts "#{current_player} is the winner!".green
+        exit
+
+      elsif check_for_draw?
+        @grid.display
+        puts "It's a draw!"
+        exit
+      end
+
+      switch_players!
+    end
+  end
+
+  def player_move(player)
+    @grid.display
+
+    puts "#{player}(#{player.marker}), please mark your move by inputting one of the letters from the grid"
+
+    move = gets.chomp.downcase
+
+    # Check for exception in user input
+    if current_values_array(@grid).include?(move) == false
+      puts 'Input is not valid. Please try again.'.red
+      player_move(player)
+    end
+
+    @grid.instance_variables.map do |value| 
+      if @grid.instance_variable_get(value) == move
+        @grid.instance_variable_set(value, player.marker)
+      end
+    end
+
+    def check_for_win?(player)
+      winning_combinations = [[@grid.a, @grid.b, @grid.c], [@grid.a, @grid.d, @grid.g], [@grid.b, @grid.e, @grid.h], [@grid.c, @grid.f, @grid.i],
+               [@grid.d, @grid.e, @grid.f], [@grid.g, @grid.h, @grid.i], [@grid.g, @grid.e, @grid.c], [@grid.a, @grid.e, @grid.i]]
+  
+      winning_combinations.any? do |combination|
+        combination.all? { |elem| elem == player.marker }
+      end
+    end
+  
+    def check_for_draw?
+      default_values_array = ("a".."i").to_a
+      (current_values_array(@grid) - default_values_array).length == 9
+    end
+  end
+
+  # this is a higher level class composed of other objects
+  # the dependencies exist upfront in the constructor
+  # it can talk to the players and the board through invoking their methods
+end
+
+Game.new(Player, Player).start_game
